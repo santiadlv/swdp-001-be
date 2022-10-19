b169d7d6c8b7449d785548936eabb8fa1df10539
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-10-18 22:22:36 -0600

    Implement SQLite and Pydantic models for data retrieval

---
+++ src/models/label_schema.py
@@ -0,0 +1,19 @@
+from typing import Union
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+
+class LabelModel(BaseModel):
+    id: int = Field(...)
+    purchase_order: int = Field(...)
+    date: datetime = Field(...)
+    description: Union[str, None] = Field(None)
+    client: str = Field(...)
+    quantity: int = Field(...)
+    supplier: str = Field(...)
+    path: Union[str, None] = Field(None)
+
+    class Config:
+        orm_mode = True
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
