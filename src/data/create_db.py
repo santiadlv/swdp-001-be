e34d916bbbbe997e6fb274d22e8aa04d71b2b40d
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-10-19 03:30:08 -0600

    Update database schema and ORM connection string

--- src/data/create_db.py
+++ src/data/create_db.py
@@ -12,21 +12,20 @@ def create_tables(cursor: sqlite3.Cursor):
         description TEXT,
         client TEXT,
         quantity INTEGER,
-        supplier TEXT,
-        path TEXT)
+        supplier TEXT)
     """)
 
 def insert_values(cursor: sqlite3.Cursor, connection: sqlite3.Connection):
-    cursor.execute('INSERT INTO labels VALUES (66926209391639, 1000, CURRENT_TIMESTAMP, "Test description 1", "Test client 1", 15, "Supplier 1", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (40901816366985, 1001, CURRENT_TIMESTAMP, "Test description 2", "Test client 2", 2, "Supplier 2", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (70620314804158, 1002, CURRENT_TIMESTAMP, "Test description 3", "Test client 3", 30, "Supplier 3", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (43812953554617, 1003, CURRENT_TIMESTAMP, "Test description 4", "Test client 4", 78, "Supplier 4", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (49602120810498, 1004, CURRENT_TIMESTAMP, "Test description 5", "Test client 5", 9, "Supplier 5", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (19399760156812, 1005, CURRENT_TIMESTAMP, "Test description 6", "Test client 6", 1, "Supplier 6", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (49052060943795, 1006, CURRENT_TIMESTAMP, "Test description 7", "Test client 7", 26, "Supplier 7", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (68785184807770, 1007, CURRENT_TIMESTAMP, "Test description 8", "Test client 8", 34, "Supplier 8", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (41050377491734, 1008, CURRENT_TIMESTAMP, "Test description 9", "Test client 9", 5, "Supplier 9", NULL)')
-    cursor.execute('INSERT INTO labels VALUES (13100553873173, 1009, CURRENT_TIMESTAMP, "Test description 10", "Test client 10", 7, "Supplier 10", NULL)')
+    cursor.execute('INSERT INTO labels VALUES (66926209391639, 1000, CURRENT_TIMESTAMP, "Test description 1", "Test client 1", 15, "Supplier 1")')
+    cursor.execute('INSERT INTO labels VALUES (40901816366985, 1001, CURRENT_TIMESTAMP, "Test description 2", "Test client 2", 2, "Supplier 2")')
+    cursor.execute('INSERT INTO labels VALUES (70620314804158, 1002, CURRENT_TIMESTAMP, "Test description 3", "Test client 3", 30, "Supplier 3")')
+    cursor.execute('INSERT INTO labels VALUES (43812953554617, 1003, CURRENT_TIMESTAMP, "Test description 4", "Test client 4", 78, "Supplier 4")')
+    cursor.execute('INSERT INTO labels VALUES (49602120810498, 1004, CURRENT_TIMESTAMP, "Test description 5", "Test client 5", 9, "Supplier 5")')
+    cursor.execute('INSERT INTO labels VALUES (19399760156812, 1005, CURRENT_TIMESTAMP, "Test description 6", "Test client 6", 1, "Supplier 6")')
+    cursor.execute('INSERT INTO labels VALUES (49052060943795, 1006, CURRENT_TIMESTAMP, "Test description 7", "Test client 7", 26, "Supplier 7")')
+    cursor.execute('INSERT INTO labels VALUES (68785184807770, 1007, CURRENT_TIMESTAMP, "Test description 8", "Test client 8", 34, "Supplier 8")')
+    cursor.execute('INSERT INTO labels VALUES (41050377491734, 1008, CURRENT_TIMESTAMP, "Test description 9", "Test client 9", 5, "Supplier 9")')
+    cursor.execute('INSERT INTO labels VALUES (13100553873173, 1009, CURRENT_TIMESTAMP, "Test description 10", "Test client 10", 7, "Supplier 10")')
     connection.commit()
 
 if __name__ == "__main__":
--- src/data/database.py
+++ src/data/database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./intersermex.db"
+DATABASE_NAME = "intersermex.db"
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(os.getcwd(), DATABASE_NAME)}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -10,3 +12,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
