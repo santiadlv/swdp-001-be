ac896b966d882e0e7def87f730759f8a8820de65
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-10-19 03:31:01 -0600

    Simplify CRUD database functions

--- src/crud/label_crud.py
+++ src/crud/label_crud.py
@@ -1,9 +1,11 @@
+from typing import Optional, List
 from sqlalchemy.orm import Session
-from ..models import label_model
+from ..models import label_schema, label_model
 
 
-def get_labels(db: Session, skip: int = 0, limit: int = 0):
-    return db.query(label_model.Label).offset(skip).limit(limit).all()
+def get_labels(db: Session) -> Optional[List[label_schema.LabelModel]]:
+    return db.query(label_model.Label).all()
+     
 
-def get_label_by_id(db: Session, label_id: int):
+def get_label_by_id(db: Session, label_id: int) -> Optional[label_schema.LabelModel]:
     return db.query(label_model.Label).filter(label_model.Label.id == label_id).first()
