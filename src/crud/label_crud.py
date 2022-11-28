9fd6de83272ec157e6960849c290ae0a485becde
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-11-27 22:15:24 -0600

    Add create and delete functionality for labels in CRUD and router modules

--- src/crud/label_crud.py
+++ src/crud/label_crud.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Optional, List
 from sqlalchemy.orm import Session
 from ..models import label_schema, label_model
@@ -5,7 +6,31 @@ from ..models import label_schema, label_model
 
 def get_labels(db: Session) -> Optional[List[label_schema.LabelModel]]:
     return db.query(label_model.Label).all()
-     
 
 def get_label_by_id(db: Session, label_id: int) -> Optional[label_schema.LabelModel]:
     return db.query(label_model.Label).filter(label_model.Label.id == label_id).first()
+
+def create_label(db: Session, obj_in: label_schema.LabelModel) -> Optional[label_schema.LabelModel]:
+    new_label = label_model.Label(
+        id=obj_in.id,
+        purchase_order=obj_in.purchase_order,
+        date=obj_in.date,
+        description=obj_in.description,
+        client=obj_in.client,
+        quantity=obj_in.quantity,
+        supplier=obj_in.supplier
+    )
+
+    db.add(new_label)
+    db.commit()
+    db.refresh(new_label)
+
+    return new_label
+
+def delete_label_by_id(db: Session, label_id: label_schema.LabelID) -> Optional[label_schema.LabelModel]:
+    label_to_delete = db.query(label_model.Label).filter(label_model.Label.id == label_id.id).first()
+    result = copy.copy(label_to_delete)
+    db.delete(label_to_delete)
+    db.commit()
+
+    return result
--- src/routers/label_router.py
+++ src/routers/label_router.py
@@ -10,6 +10,8 @@ from ..utils import encoder
 
 from typing import List
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import UnmappedInstanceError
 
 
 router = APIRouter(
@@ -39,4 +41,31 @@ def get_label(db: Session = Depends(get_db), id: str = Path(..., title="ID of la
     return JSONResponse(status_code=status.HTTP_200_OK, content={
         "message": "Label retrieved successfully",
         "data": label
+    })
+
+@router.post("/new", response_description="Create a new label in database", response_model=label_schema.LabelModel)
+def create_label(label_in: label_schema.LabelModel, db: Session = Depends(get_db)) -> JSONResponse:
+    try:
+        new_label = jsonable_encoder(label_crud.create_label(db, label_in))
+    except IntegrityError:
+        raise HTTPException(status_code=409, detail=f"There is already a label with provided unique ID. UNIQUE constraint failed at insert operation.")
+
+    if new_label is None:
+        raise HTTPException(status_code=422, detail="Label could not be created.")
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content={
+        "message": "Label created successfully.",
+        "data": new_label
+    })
+
+@router.post("/delete", response_description="Delete an existing label by id", response_model=label_schema.LabelModel)
+def delete_label_by_id(label_id: label_schema.LabelID, db: Session = Depends(get_db)) -> JSONResponse:
+    try:
+        deleted_label = jsonable_encoder(label_crud.delete_label_by_id(db, label_id))
+    except UnmappedInstanceError:
+        raise HTTPException(status_code=404, detail=f"Label with ID {label_id.id} could not be found, deletion aborted.")
+    
+    return JSONResponse(status_code=status.HTTP_202_ACCEPTED, content={
+        "message": f"Label with ID {label_id.id} successfully deleted.",
+        "data": deleted_label
     })
