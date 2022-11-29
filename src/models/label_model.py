8b9931fff140e0b689da81f63ff55c23325b525b
santiadlv <49079265+santiadlv@users.noreply.github.com>
2022-11-28 20:51:20 -0600

    Remove automatic ID generation, create ID and date validators, modify template model

--- src/models/label_model.py
+++ src/models/label_model.py
@@ -8,7 +8,7 @@ class Label(Base):
     __tablename__ = "labels"
 
     id = Column(Integer, primary_key=True, index=True, nullable=False)
-    purchase_order = Column(Integer, unique=True, index=True)
+    purchase_order = Column(Integer)
     date = Column(DateTime, server_default=func.now())
     description = Column(String)
     client = Column(String)
--- src/models/label_schema.py
+++ src/models/label_schema.py
@@ -1,17 +1,31 @@
+import math
 from typing import Union
 from datetime import datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 from ..utils import id_gen
 
 class LabelModel(BaseModel):
-    id: int = Field(default_factory=id_gen.generate_pk)
+    id: int = Field(...)
     purchase_order: int = Field(...)
     date: datetime = Field(default_factory=datetime.utcnow)
     description: Union[str, None] = Field(None)
     client: str = Field(...)
     quantity: int = Field(...)
     supplier: str = Field(...)
+
+    @validator('id', pre=True)
+    def parse_id(cls, v):
+        assert isinstance(v, int), "ID must be a number"
+        assert int(math.log10(v)) + 1 == 14, "ID length must be 14 digits for barcode generation"
+        return v
+
+    @validator('date', pre=True)
+    def parse_date(cls, v):
+        if isinstance(v, str):
+            return datetime.strptime(v[:10], "%Y-%m-%d")
+        return v
+
 
     class Config:
         orm_mode = True
@@ -20,6 +34,12 @@ class LabelModel(BaseModel):
 
 class LabelID(BaseModel):
     id: int = Field(...)
+
+    @validator('id', pre=True)
+    def parse_id(cls, v):
+        assert isinstance(v, int), "ID must be a number"
+        assert int(math.log10(v)) + 1 == 14, "ID length must be 14 digits for barcode generation"
+        return v
 
     class Config:
         orm_mode = True
